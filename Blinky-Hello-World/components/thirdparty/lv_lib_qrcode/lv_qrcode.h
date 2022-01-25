426da5496066a3c879898c9c2a97882191fc4066
David Walters <waltrsd@amazon.com>
2022-01-25 18:24:44 -0500

    Lower version of QR code lib to v7

--- Blinky-Hello-World/components/thirdparty/lv_lib_qrcode/lv_qrcode.h
+++ Blinky-Hello-World/components/thirdparty/lv_lib_qrcode/lv_qrcode.h
@@ -14,9 +14,9 @@ extern "C" {
  *      INCLUDES
  *********************/
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
-#include "lvgl.h"
+#include <lvgl.h>
 #else
-#include "lvgl/lvgl.h"
+#include <lvgl/lvgl.h>
 #endif
 /*********************
  *      DEFINES
